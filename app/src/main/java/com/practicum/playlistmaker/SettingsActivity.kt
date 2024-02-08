768145eb995512f42295ba51de1a91b818b9b15f
Мyachkaa <myachkaaa@ya.ru>
2024-02-08 20:06:55 +0300

    Домашнее задание 2: - Стили и темы - Context и Intent - EditText и TextWatcher - Хранение данных. Начало

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,10 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
@@ -10,9 +13,30 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val back = findViewById<ImageView>(R.id.back)
+        val share = findViewById<FrameLayout>(R.id.share)
+        val support = findViewById<FrameLayout>(R.id.support)
+        val agreement = findViewById<FrameLayout>(R.id.agreement)
 
         back.setOnClickListener {
             finish()
+        }
+        share.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_link))
+            shareIntent.type = "text/plain"
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.share)))
+        }
+        support.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            supportIntent.data = Uri.parse("mailto:")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_mail)))
+            supportIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.message))
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.subject_mail))
+            startActivity(supportIntent)
+        }
+        agreement.setOnClickListener {
+            val agreementIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.agreement_link)))
+            startActivity(agreementIntent)
         }
     }
 }
