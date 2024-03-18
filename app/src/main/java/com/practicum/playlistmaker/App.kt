dd9f6dd071ef950bdaae597222717f13146ad40f
Мyachkaa <myachkaaa@ya.ru>
2024-03-18 14:33:17 +0300

    Спринт 12. -Добавление истории поиска -Настройка переключателя темной темы

---
+++ app/src/main/java/com/practicum/playlistmaker/App.kt
@@ -0,0 +1,42 @@
+package com.practicum.playlistmaker
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+class App : Application() {
+
+    private var darkTheme = false
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val sharedPrefs = getSharedPreferences(THEME_SWITCHER, MODE_PRIVATE)
+        darkTheme = sharedPrefs.getBoolean(SWITCHER_KEY, false)
+        switchTheme(darkTheme)
+    }
+
+
+    fun switchTheme(darkThemeEnabled: Boolean) {
+        saveThemeState(darkThemeEnabled)
+        darkTheme = darkThemeEnabled
+        AppCompatDelegate.setDefaultNightMode(
+            if (darkThemeEnabled) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+        )
+    }
+    private fun saveThemeState(darkThemeEnabled: Boolean) {
+        val sharedPrefs = getSharedPreferences(THEME_SWITCHER, MODE_PRIVATE)
+        sharedPrefs.edit()
+        .putBoolean(SWITCHER_KEY, darkThemeEnabled)
+        .apply()
+    }
+
+
+    companion object {
+        const val THEME_SWITCHER = "practicum_playlistmaker_theme_switcher"
+        const val SWITCHER_KEY = "key_for_theme_switcher"
+    }
+}
--- app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
@@ -1,18 +1,24 @@
 package com.practicum.playlistmaker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TrackAdapter : RecyclerView.Adapter<TracksViewHolder> () {
 
     var tracks = ArrayList<Track>()
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TracksViewHolder = TracksViewHolder(parent)
+    var onItemClick: ((Track) -> Unit)? = null
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TracksViewHolder {
+        val viewHolder = LayoutInflater.from(parent.context).inflate(R.layout.track_layout, parent, false)
+        return TracksViewHolder(viewHolder)
+    }
 
     override fun onBindViewHolder(holder: TracksViewHolder, position: Int) {
-        holder.bind(tracks.get(position))
+        holder.bind(tracks[position])
+        holder.itemView.setOnClickListener {
+            onItemClick?.invoke(tracks[position])
+        }
     }
 
     override fun getItemCount(): Int = tracks.size
-
-
 }
